0bc836fdd3c71325007629331469417ef8cc4258
IN <devildogmanman@gmail.com>
2026-04-16 14:57:25 +0700

    Fix: bring back all source code into repository

---
+++ Food Tinder/Food_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_TinderApp.swift
+//  Food Tinder
+//
+//  Created by INSU on 14/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct Food_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
